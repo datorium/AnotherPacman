6ddebab169b08f59f002a32c6745d67f94327992
Datorium <hello@datorium.lv>
2020-08-15 16:28:53 +0300

    version 16:28

---
+++ Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnotherPacman
+{
+    public partial class Game : Form
+    {
+        private Level level = new Level();
+
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            //adjust game form size
+            this.Size = new Size(500, 500);
+            //adding level to the game
+            this.Controls.Add(level);
+
+        }
+    }
+}
