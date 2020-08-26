b130d90689232666bbdd629d6790135e2d012c60
Datorium <hello@datorium.lv>
2020-08-26 16:30:01 +0300

    version 16:29

---
+++ Score.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AnotherPacman
+{
+    class Score : PictureBox
+    {
+        public string scoreType { get; private set; } = null;
+        public Score(int score)
+        {
+            this.scoreType = "score_" + score.ToString();
+            InitializeScore();
+        }
+
+        private void InitializeScore()
+        {
+            this.BackColor = Color.Transparent;
+            this.Size = new Size(30, 15);
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Image = (Image)Properties.Resources.ResourceManager.GetObject(this.scoreType);
+        }
+    }
+}
